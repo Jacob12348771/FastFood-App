27af03d157364eafe3224e331db3a98539fd1e31
BryceReading <swag.uo@gmail.com>
2025-09-07 19:52:43 +0100

    Search page v1

---
+++ components/MenuCard.tsx
@@ -0,0 +1,15 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { MenuItem } from '@/type'
+import { appwriteConfig } from '@/lib/appwrite';
+
+const MenuCard = ({ item: {image_url, name, price}}: {item:MenuItem}) => {
+    const imageUrl = `${image_url}?project=${appwriteConfig.projectId}`;
+  return (
+    <TouchableOpacity>
+        <Image source={{uri: imageUrl}} className="size-32 absolute -top-10" resizeMode="contain" />
+        <Text className="text-center base-bold text-dark-100 mb-2" numberOfLines={1}>{name}</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default MenuCard
